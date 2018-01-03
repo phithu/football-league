5953f45f82bfe9ec479d49f1fddef869a1ffbe02
phithu <phithu.dev@gmail.com>
2018-01-03 09:13:40 +0700

    update insert team

--- src/config/app-const.ts
+++ src/config/app-const.ts
@@ -1,6 +1,6 @@
 export const AppConst = {
-  // domain: 'https://vn-league.herokuapp.com',
+  domain: 'https://vn-league.herokuapp.com',
   // java: 'https://cors-anywhere.herokuapp.com/https://vn-league.azurewebsites.net/rest',
-  domain: 'http://localhost:3000',
+  // domain: 'http://localhost:3000',
   java: 'https://vn-league-api.herokuapp.com/rest'
 };
